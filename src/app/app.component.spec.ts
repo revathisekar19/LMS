3c5fc55a77fed9f1fb7b56a32591e0ad5ecf1e7a
Revathi Sekar <revathisekar2017@gmail.com>
2024-11-13 16:42:15 -0500

    updated docker file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CourseComponent } from './components/course/course.component';
+import { EditcourseComponent } from './components/editcourse/editcourse.component';
+import { EnrollComponent } from './components/enrollcourse/enroll.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ViewcourseComponent } from './components/viewcourse/viewcourse.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        CommonModule,
+        RouterModule.forRoot([]), 
+        MatDialogModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+   CourseComponent,
+   DashboardComponent,
+   EnrollComponent,
+   ViewcourseComponent,
+   ProfileComponent,
+   NavbarComponent,
+   HomeComponent,
+   FooterComponent,
+   RegisterComponent,
+   EditcourseComponent
       ],
+      
     }).compileComponents();
   });
 
