ea7dccfb9fd47c4ddcb467c9b2ed9916c0840383
Revathi Sekar <revathisekar2017@gmail.com>
2024-11-13 17:09:46 -0500

    added schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -71,6 +71,7 @@ import { EditcourseComponent } from './components/editcourse/editcourse.componen
     MatTableModule,
     MatDialogModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
     provideAnimationsAsync()
   ],
